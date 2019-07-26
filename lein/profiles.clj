f5e57012bec578e52230d3924d6e45feec4b8c9a
Drew Inglis <drew@drewinglis.com>
2019-07-25 17:19:03 -0700

    Add lein refresh alias for lein monolith

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0" :exclusions [org.clojure/tools.nrepl]]
-                  [lein-monolith "1.2.0"]]}}
+{:user {:plugins [;[cider/cider-nrepl "0.21.1" :exclusions [org.clojure/tools.nrepl]]
+                  [lein-monolith "1.2.0"]]
+        :aliases {"refresh" ["do" "monolith" "each" ":refresh" "build"
+                             ":upstream" ":skip" :project/name ":parallel"
+                             "4" "install,"]}}}
