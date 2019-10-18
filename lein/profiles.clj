262b7952e0579646e412e08d6e57fc1d0e9b3272
Drew Inglis <drew@drewinglis.com>
2019-10-17 18:12:31 -0700

    grab bag of fixes. too lazy for commit msg

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,16 @@
-{:user {:plugins [;[cider/cider-nrepl "0.21.1" :exclusions [org.clojure/tools.nrepl]]
+{:user {:plugins [[mvxcvi/whidbey "2.1.1"]
+                  [com.jakemccrary/lein-test-refresh "0.24.1"]
                   [lein-monolith "1.2.0"]]
         :aliases {"refresh" ["do" "monolith" "each" ":refresh" "build"
                              ":upstream" ":skip" :project/name ":parallel"
-                             "4" "install,"]}}}
+                             "4" "install,"]}
+        :test-refresh {:notify-command ["/usr/local/bin/terminal-notifier"
+                                        "-group" "lein-test-refresh" "-title"
+                                        "lein test-refresh" "-message"]
+                       :changes-only true}
+        :middleware [whidbey.plugin/repl-pprint]
+        :whidbey {:width 180
+                  :map-delimiter ""
+                  :extend-notation true
+                  :print-meta true
+                  :color-scheme {:nil [:blue]}}}}
