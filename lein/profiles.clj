2d6acbe751dd908147f687121a928dde49e2f277
Drew Inglis <drew@drewinglis.com>
2020-05-11 14:32:00 -0700

    lein profile updates

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,14 +1,17 @@
-{:user {:plugins [[mvxcvi/whidbey "2.1.1"]
-                  [com.jakemccrary/lein-test-refresh "0.24.1"]
-                  [lein-monolith "1.2.0"]]
-        :aliases {"refresh" ["do" "monolith" "each" ":refresh" "build"
+{:user {:aliases {"refresh" ["do" "monolith" "each" ":refresh" "build"
                              ":upstream" ":skip" :project/name ":parallel"
                              "4" "install,"]}
+        :dependencies [[hashp "0.1.1"]]
+        :injections [(require 'hashp.core)]
+        ;;:middleware [whidbey.plugin/repl-pprint]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                  [lein-ancient "0.6.15"]
+                  [lein-monolith "1.4.0"]
+                  [mvxcvi/whidbey "2.1.1"]]
         :test-refresh {:notify-command ["/usr/local/bin/terminal-notifier"
                                         "-group" "lein-test-refresh" "-title"
                                         "lein test-refresh" "-message"]
                        :changes-only true}
-        :middleware [whidbey.plugin/repl-pprint]
         :whidbey {:width 180
                   :map-delimiter ""
                   :extend-notation true
